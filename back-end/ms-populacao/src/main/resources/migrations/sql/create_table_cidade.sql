687e3cba9da8b0efd9edceb2a14866b69c9a172b
Waldecleber <waldecleber@gmail.com>
2020-03-30 15:19:57 -0300

    Create db-migration projeto to liquibase

--- back-end/ms-populacao/src/main/resources/migrations/sql/create_table_cidade.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS cidade (
-        id bigint not null auto_increment,       
-        nome varchar(255),
-        populacao int,
-        id_estado  bigint,
-        primary key (id),
-        FOREIGN KEY (`id_estado`) REFERENCES `estado` (`id`)
-    ) engine=InnoDB
